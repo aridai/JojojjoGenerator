d49224cb67e8ca3b7ee851666e4c1f60c40101df
aridai <aridai.net@outlook.jp>
2022-06-21 08:37:39 +0900

    ktlintで指摘が入るソースコードを追加

--- src/main/kotlin/net/aridai/jojojjogenerator/Main.kt
+++ src/main/kotlin/net/aridai/jojojjogenerator/Main.kt
@@ -6,6 +6,9 @@ import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
 
+ class BadClass(val x: Int,
+val  y: String )
+
 fun main() {
     renderComposable(rootElementId = "root") {
         Body()
